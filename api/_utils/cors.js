a3dc0ae7c33cfad6a745215d9a7feaa86ee2e018
Vexcompany <vexcorporation43@gmail.com>
2026-03-09 05:15:00 +0700

    Convert CORS headers and handler to CommonJS module

--- api/_utils/cors.js
+++ api/_utils/cors.js
@@ -1,12 +1,12 @@
-export const corsHeaders = {
+module.exports.corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type',
   'Content-Type': 'application/json'
 };
 
-export function handleCors(res) {
-  Object.entries(corsHeaders).forEach(([key, value]) => {
+module.exports.handleCors = function(res) {
+  Object.entries(this.corsHeaders).forEach(([key, value]) => {
     res.setHeader(key, value);
   });
-}
+};
